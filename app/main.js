e2200274b0fb23288481bb0d84f54afdf7b4fac5
Tom Lewis <tom@ricochetpartners.com>
2018-11-13 17:47:39 -0800

    init

---
+++ app/main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow();
+
+  mainWindow.loadFile('app/index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
